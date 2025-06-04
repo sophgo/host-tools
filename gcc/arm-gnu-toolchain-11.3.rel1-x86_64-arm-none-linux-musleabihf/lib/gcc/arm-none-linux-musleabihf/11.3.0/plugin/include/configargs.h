ad5193673b393ddaadb86e85403a025269e86d11
sophgo-forum-service <forum_service@sophgo.com>
2025-06-04 11:02:51 +0800

    host-tools: upload arm-none-linux-musleabihf

---
+++ gcc/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-musleabihf/lib/gcc/arm-none-linux-musleabihf/11.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/project/buildroot/output/host --sysconfdir=/project/buildroot/output/host/etc --enable-static --target=arm-none-linux-musleabihf --with-sysroot=/project/buildroot/output/host/arm-none-linux-musleabihf/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --enable-plugins --enable-lto --with-gmp=/project/buildroot/output/host --with-mpc=/project/buildroot/output/host --with-mpfr=/project/buildroot/output/host --with-pkgversion='Buildroot 2023.02-rc3' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libmpx --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --enable-tls --enable-threads --without-isl --without-cloog --with-abi=aapcs-linux --with-cpu=cortex-a53 --with-fpu=neon-vfpv4 --with-float=hard --with-mode=thumb --enable-languages=c,c++,fortran --with-build-time-tools=/project/buildroot/output/host/arm-none-linux-musleabihf/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a53" }, { "float", "hard" }, { "mode", "thumb" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
